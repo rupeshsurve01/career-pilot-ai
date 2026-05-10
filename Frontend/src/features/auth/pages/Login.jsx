cc3acebd53562819746653111f4d88aea681c1fd
Rupesh <rupeshsurve302@gmail.com>
2026-05-10 14:48:23 +0530

    feat: implement register UI

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,38 +1,48 @@
-import React from 'react'
-import "../auth.form.scss"
+import React from "react";
+import "../auth.form.scss";
+import { useNavigate, Link } from "react-router";
 
 const Login = () => {
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        // e.priventDefault()
-    }
-
+  const handleSubmit = (e) => {
+    e.priventDefault();
+  };
 
   return (
-    <>
     <main>
-        <div className="form-container">
-            <h1>Login</h1>
+      <div className="form-container">
+        <h1>Login</h1>
 
-            <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter Email Address"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Password"
+            />
+          </div>
 
-                <div className="input-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder='Enter Email Address' />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder='Enter Email Password' />
-                </div>
+          <button className="button primary-button">Login</button>
+        </form>
 
-                <button className='button primary-button'>Login</button>
-
-            </form>
-        </div>
+        <p>
+          Don't have an Account? <Link to={"/register"}>Register</Link>
+        </p>
+      </div>
     </main>
-    
-    </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { useNavigate,Link } from 'react-router'
 
 const Register = () => {
+
+  const navigate = useNavigate()
+  
+    const handleSubmit = (e) => {
+        e.priventDefault()
+    }
+
+
   return (
-    <div>
-      Register
-    </div>
+     <main>
+        <div className="form-container">
+            <h1>Register</h1>
+
+            <form onSubmit={handleSubmit}>
+
+                <div className="input-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" placeholder='Enter Username ' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder='Enter Email Address' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder='Enter Password' />
+                </div>
+
+                <button className='button primary-button'>Register</button>
+
+            </form>
+
+            <p>Alreday have an Account? <Link to={"/login"}>Login</Link></p>
+        </div>
+    </main>
   )
 }
 
