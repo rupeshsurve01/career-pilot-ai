a51c3dbac08279cdb62608f1d811bcfeef70cd12
Rupesh <rupeshsurve302@gmail.com>
2026-05-13 17:00:57 +0530

    feat: add protected component to protect routes

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import React from "react";
 import { useNavigate, Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 
 const Register = () => {
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loading, handleRegister } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await handleRegister({ username, email, password });
+    navigate("/");
+
+    if (loading) {
+      return (
+        <main>
+          <h1>Loading......</h1>
+        </main>
+      );
+    }
   };
 
   return (
@@ -17,6 +35,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="username">Username</label>
             <input
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
               type="text"
               name="username"
               id="username"
@@ -26,6 +47,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               type="email"
               name="email"
               id="email"
@@ -35,6 +59,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
               type="password"
               name="password"
               id="password"
