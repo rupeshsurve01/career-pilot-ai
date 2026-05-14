875e0aaff69badde427828c2472f79f3a0b00690
Rupesh <rupeshsurve302@gmail.com>
2026-05-14 20:24:05 +0530

    feat: add interview report generation with Zod validation

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();// used to access environment variables from a .env file into process.env
 const app = require('./src/app');
 const connectDB = require('./src/config/database');
-const invokeGeminiAi = require('./src/services/ai.service')
 
 connectDB();
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
