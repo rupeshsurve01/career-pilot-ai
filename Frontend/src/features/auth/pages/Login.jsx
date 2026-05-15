f247e5d7e7b4f50dca658faa52ad29d35a95c8ef
Rupesh <rupeshsurve302@gmail.com>
2026-05-15 20:59:06 +0530

    refactor: streamline Login component and improve code readability

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,72 +1,50 @@
-import React, { useState } from "react";
-import "../auth.form.scss";
-import { useNavigate, Link } from "react-router";
-import { useAuth } from "../hooks/useAuth";
+import React,{useState} from 'react'
+import { useNavigate, Link } from 'react-router'
+import "../auth.form.scss"
+import { useAuth } from '../hooks/useAuth'
 
 const Login = () => {
-  const { loading, handleLogin } = useAuth();
-  const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
+    const { loading, handleLogin } = useAuth()
+    const navigate = useNavigate()
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await handleLogin({ email, password: Password });
-    if (data?.user) {
-      navigate("/");
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await handleLogin({email,password})
+        navigate('/')
     }
-  };
 
-  if (loading) {
+    if(loading){
+        return (<main><h1>Loading.......</h1></main>)
+    }
+
+
     return (
-      <main>
-        <h1>Loading......</h1>
-      </main>
-    );
-  }
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            type="email" id="email" name='email' placeholder='Enter email address' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type="password" id="password" name='password' placeholder='Enter password' />
+                    </div>
+                    <button className='button primary-button' >Login</button>
+                </form>
+                <p>Don't have an account? <Link to={"/register"} >Register</Link> </p>
+            </div>
+        </main>
+    )
+}
 
-  return (
-    <main>
-      <div className="form-container">
-        <h1>Login</h1>
-
-        <form onSubmit={handleSubmit}>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter Email Address"
-            />
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Enter Password"
-            />
-          </div>
-
-          <button className="button primary-button">Login</button>
-        </form>
-
-        <p>
-          Don't have an Account? <Link to={"/register"}>Register</Link>
-        </p>
-      </div>
-    </main>
-  );
-};
-
-export default Login;
+export default Login
