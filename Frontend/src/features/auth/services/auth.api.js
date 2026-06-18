e5213b8d94ec10c4adc15d37a44ce2666aee8812
Rupesh <rupeshsurve302@gmail.com>
2026-06-18 18:01:47 +0530

    refactor: Remove error handling from auth API functions for cleaner code

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -6,48 +6,33 @@ const api = axios.create({
 });
 
 export async function register({ username, email, password }) {
-  try {
-    const response = await api.post("/api/auth/register", {
-      username,
-      email,
-      password,
-    });
+  const response = await api.post("/api/auth/register", {
+    username,
+    email,
+    password,
+  });
 
-    return response.data;
-  } catch (error) {
-    console.log("error:", error);
-  }
+  return response.data;
 }
 
 export async function login({ email, password }) {
-  try {
-    const response = await api.post("/api/auth/login", {
-      email,
-      password,
-    });
+  const response = await api.post("/api/auth/login", {
+    email,
+    password,
+  });
 
-    return response.data;
-  } catch (error) {
-    console.log("error:", error);
-  }
+  return response.data;
 }
 
 export async function logout() {
-  try {
-    const response = await api.get("/api/auth/logout");
+  const response = await api.get("/api/auth/logout");
 
-    return response.data;
-  } catch (error) {
-    console.log("error:", error);
-  }
+  return response.data;
 }
 
 export async function getMe() {
-  try {
-    const response = await api.get("/api/auth/get-me");
+  const response = await api.get("/api/auth/get-me");
 
-    return response.data;
-  } catch (error) {
-    console.log("error:", error);
-  }
+  return response.data;
 }
+
